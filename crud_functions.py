2c560f221cfd6e161452c11a10481f04936ad1f7
alinaand97 <alinaand_97@mail.ru>
2024-12-19 09:39:04 +1000

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def initiate_db():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            price INTEGER NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def get_all_products():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT title, description, price FROM Products')
+    products = cursor.fetchall()
+    conn.close()
+    return products
+
